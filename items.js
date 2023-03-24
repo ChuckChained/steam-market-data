36b4a1d7844002e399cdae5b24938ccc4265eecd
ChuckChained <chuckchained@gmail.com>
2023-03-24 03:50:03 +0000

    Include all items

--- items.js
+++ items.js
@@ -2,56 +2,56 @@ export let items = [
 
 	// Operation Cases
 	"Operation%20Riptide%20Case",
-	"Operation%20Broken%20Fang%20Case"
-	// "Shattered%20Web%20Case",
-	// "Operation%20Hydra%20Case",
-	// "Operation%20Wildfire%20Case",
-	// "Falchion%20Case",
-	// "Operation%20Vanguard%20Weapon%20Case",
-	// "Operation%20Breakout%20Weapon%20Case", 
-	// "Operation%20Phoenix%20Weapon%20Case",
-	// "Operation%20Bravo%20Case",
+	"Operation%20Broken%20Fang%20Case",
+	"Shattered%20Web%20Case",
+	"Operation%20Hydra%20Case",
+	"Operation%20Wildfire%20Case",
+	"Falchion%20Case",
+	"Operation%20Vanguard%20Weapon%20Case",
+	"Operation%20Breakout%20Weapon%20Case", 
+	"Operation%20Phoenix%20Weapon%20Case",
+	"Operation%20Bravo%20Case",
 
-	// // Weapon Cases
-	// "CS:GO%20Weapon%20Case",
-	// "CS:GO%20Weapon%20Case%202",
-	// "CS:GO%20Weapon%20Case%203",
+	// Weapon Cases
+	"CS:GO%20Weapon%20Case",
+	"CS:GO%20Weapon%20Case%202",
+	"CS:GO%20Weapon%20Case%203",
 
-	// // eSports Cases
-	// "eSports%202013%20Case",
-	// "eSports%202013%20Winter%20Case",
-	// "eSports%202014%20Summer%20Case",
+	// eSports Cases
+	"eSports%202013%20Case",
+	"eSports%202013%20Winter%20Case",
+	"eSports%202014%20Summer%20Case",
 
-	// // Chroma Cases
-	// "Chroma%20Case",
-	// "Chroma%202%20Case",
-	// "Chroma%203%20Case",
+	// Chroma Cases
+	"Chroma%20Case",
+	"Chroma%202%20Case",
+	"Chroma%203%20Case",
 
-	// // Gamma Cases
-	// "Gamma%20Case",
-	// "Gamma%202%20Case",
+	// Gamma Cases
+	"Gamma%20Case",
+	"Gamma%202%20Case",
 
-	// // Spectrum Cases
-	// "Spectrum%20Case",
-	// "Spectrum%202%20Case",
+	// Spectrum Cases
+	"Spectrum%20Case",
+	"Spectrum%202%20Case",
 
-	// // Prisma Cases
-	// "Prisma%20Case",
-	// "Prisma%202%20Case",
+	// Prisma Cases
+	"Prisma%20Case",
+	"Prisma%202%20Case",
 
-	// "CS20%20Case",
-	// "Revolution%20Case",
-	// "Dreams%20%26%20Nightmares%20Case",
-	// "Recoil%20Case",
-	// "Clutch%20Case",
-	// "Fracture%20Case",
-	// "Snakebite%20Case",
-	// "Danger%20Zone%20Case",
-	// "Horizon%20Case",
-	// "Shadow%20Case",
-	// "Glove%20Case",
-	// "Winter%20Offensive%20Weapon%20Case",
-	// "Huntsman%20Weapon%20Case",
-	// "Revolver%20Case"
+	"CS20%20Case",
+	"Revolution%20Case",
+	"Dreams%20%26%20Nightmares%20Case",
+	"Recoil%20Case",
+	"Clutch%20Case",
+	"Fracture%20Case",
+	"Snakebite%20Case",
+	"Danger%20Zone%20Case",
+	"Horizon%20Case",
+	"Shadow%20Case",
+	"Glove%20Case",
+	"Winter%20Offensive%20Weapon%20Case",
+	"Huntsman%20Weapon%20Case",
+	"Revolver%20Case"
 
 ]
